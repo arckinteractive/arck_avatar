7ab8da8f5813ed0d1c4b3fd4c6bef09b81d12d71
Matt Beckett <beck24@gmail.com>
2020-03-21 00:48:08 -0700

    Add language string for too-large-image error

--- languages/en.php
+++ languages/en.php
@@ -2,5 +2,5 @@
 
 return array(
 	'arck:avatar:invalid_format' => 'This file type is not supported',
-	
+	'arck:avatar:image_too_large' => 'The uploaded image is too large to resize, please try a smaller version'
 );
